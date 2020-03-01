70708dcb1ad5dc13bdd9d42624081066f8d26a13
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-02-29 21:26:19 -0600

    Fetch en funcionamiento

--- api/start/routes.js
+++ api/start/routes.js
@@ -23,4 +23,6 @@ Route.get('/', () => {
 Route.group(() => {
   Route.post('users/register', 'UserController.store')
   Route.post('users/login', 'UserController.login')
+  Route.get('users/testRick', 'UserController.testRick')
+  Route.get('users/testFetch', 'UserController.testFetch')
 }).prefix('api/v1')
